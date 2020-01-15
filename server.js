41b4cc2dafc268af625a64ae512b480faa56d84c
Mike <mikeyobviously@gmail.com>
2020-01-15 15:16:28 -0800

    able to register users

--- server.js
+++ server.js
@@ -4,8 +4,31 @@ const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
+const User = require('./models/User');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 require('dotenv').config();
 require('./config/database');
+
+
+// POST route to register a user
+app.post('/api/register', function(req, res) {
+  const { email, password } = req.body;
+  const user = new User({ email, password });
+  user.save(function(err) {
+    if (err) {
+      res.status(500)
+        .send("Error registering new user please try again.");
+    } else {
+      res.status(200).send("Welcome to the club!");
+    }
+  });
+});
 
 app.get('/secret', function (req, res) {
   return res.send('sauce');
@@ -17,6 +40,8 @@ app.get('/api/home', function(req, res) {
 app.get('/api/secret', function(req, res) {
   return res.send('The password is potato');
 });
+
+
 
 app.get('/ping', function (req, res) {
  return res.send('pong');
