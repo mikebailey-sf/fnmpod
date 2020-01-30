c3b10185f24aca32e46d0a4a7e889a130172d356
Mike <mikeyobviously@gmail.com>
2020-01-29 19:22:07 -0800

    working on creating a year of dates, need to make save() async

--- controllers/api.js
+++ controllers/api.js
@@ -9,11 +9,13 @@ module.exports = {
 function createDates(req, res){
   let date = new Date('2020-1-31');
   for (i = 1; i < 52; i++){
-    date.setDate(date.getDate() + (7 * i));    
+    date.setDate(date.getDate() + 7);  
+    //console.log(date);
     let friday = new Friday({
       date: date
     });
-    friday.save();
+    console.log(friday);
+    //friday.save();
   }
   return res.send('Dates Created');
 }
--- models/Friday.js
+++ models/Friday.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const FridaySchema = new mongoose.Schema({
   date: Date,
-  players: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+  players: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}]
 });
 
 module.exports = mongoose.model('Friday', FridaySchema);
